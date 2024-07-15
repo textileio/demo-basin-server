51eeb578f7d160f8561bd66fddd1b0b84d1df0a6
Dan Buchholz <dbuchholz30@gmail.com>
2024-07-15 08:53:38 -0400

    refactor: fix multipart parsing | add os contract flow

--- src/server/set.rs
+++ src/server/set.rs
@@ -1,6 +1,8 @@
-use std::error::Error;
 use std::ops::Deref;
+use std::str::FromStr;
+use std::{error::Error, os::unix::net};
 
+use adm_sdk::machine::Machine;
 use bytes::Buf;
 use ethers::prelude::TransactionReceipt;
 use fendermint_crypto::SecretKey;
@@ -8,12 +10,17 @@ use futures::TryStreamExt;
 use fvm_shared::{address::Address, econ::TokenAmount};
 use serde::Deserialize;
 use serde_json::json;
+use tokio::fs::File;
+use tokio::io::{AsyncSeekExt, AsyncWriteExt};
 use warp::{
     multipart::{FormData, Part},
     Filter, Rejection, Reply,
 };
 
-use adm_sdk::{account::Account, network::Network as SdkNetwork};
+use adm_provider::{json_rpc::JsonRpcProvider, response::Cid, tx::TxReceipt, util::parse_address};
+use adm_sdk::{
+    account::Account, machine::objectstore::ObjectStore, network::Network as SdkNetwork,
+};
 
 use crate::server::{
     shared::{get_faucet_wallet, with_private_key, BadRequest, BaseRequest},
@@ -22,14 +29,6 @@ use crate::server::{
 
 /// Maximum file size for uploaded files.
 const MAX_FILE_SIZE: u64 = 1024 * 1024 * 100; // 100 MB
-
-/// Setting a value on the subnet (string or file).
-#[derive(Deserialize)]
-#[serde(untagged)]
-pub enum SetValue {
-    String(String),
-    File(FileData),
-}
 
 #[derive(Deserialize)]
 pub struct FileData {
@@ -42,24 +41,29 @@ pub struct FileData {
 pub struct SetRequest {
     #[serde(flatten)]
     pub base: BaseRequest,
-    /// The value to set at the given key.
-    pub value: SetValue,
+    /// The value to set at the given key (string or file).
+    pub value: FileData,
 }
 
-impl std::fmt::Display for SetRequest {
+impl std::fmt::Display for FileData {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match &self.value {
-            SetValue::String(s) => write!(f, "{}, value: {}", self.base, s),
-            SetValue::File(file) => write!(
-                f,
-                "{}, file: {}, size: {} bytes",
-                self.base,
-                file.filename,
-                file.content.len()
-            ),
-        }
+        write!(f, "filename: {}, content: {}", self.filename, "test str")
     }
 }
+// fmt::Display for SetRequest {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         match &self.value {
+//             SetValue::String(s) => write!(f, "{}, value: {}", self.base, s),
+//             SetValue::File(file) => write!(
+//                 f,
+//                 "{}, file: {}, size: {} bytes",
+//                 self.base,
+//                 file.filename,
+//                 file.content.len()
+//             ),
+//         }
+//     }
+// }
 
 impl Deref for SetRequest {
     type Target = BaseRequest;
@@ -97,39 +101,72 @@ async fn validate_content_type(
 }
 
 pub async fn handle_set(form: FormData, private_key: SecretKey) -> Result<impl Reply, Rejection> {
-    let mut fields = Vec::new();
-    let mut file_content = Vec::new();
+    let network = SdkNetwork::Testnet.init();
+    let os_addr = parse_address("t2ymaz2yovxlqplqd53tfuiw4umwpdt7tfmbf3v7q").unwrap();
+    let os = ObjectStore::attach(os_addr);
+
+    let mut filename: Option<String> = None;
+    let mut file_content: Vec<u8> = Vec::new();
+    let mut address: Option<Address> = None;
+    let mut key: Option<String> = None;
 
     let mut parts = form;
 
     while let Ok(Some(mut part)) = parts.try_next().await {
-        let name = part.name().to_string();
-
-        let mut bytes: Vec<u8> = Vec::new();
-        while let Some(content) = part.data().await {
-            match content {
-                Ok(content) => {
-                    bytes.extend_from_slice(content.chunk());
-                }
-                Err(e) => {
-                    eprintln!("form error: {}", e);
-                    return Err(warp::reject::reject());
+        let field = part.name().to_string();
+        if field == "file" {
+            filename = part.filename().map(String::from);
+            while let Some(content) = part.data().await {
+                match content {
+                    Ok(content) => {
+                        file_content.extend_from_slice(content.chunk());
+                    }
+                    Err(e) => {
+                        eprintln!("form error: {}", e);
+                        return Err(warp::reject::reject());
+                    }
                 }
             }
+        } else {
+            let mut bytes: Vec<u8> = Vec::new();
+            while let Some(content) = part.data().await {
+                match content {
+                    Ok(content) => {
+                        bytes.extend_from_slice(content.chunk());
+                    }
+                    Err(e) => {
+                        eprintln!("form error: {}", e);
+                        return Err(warp::reject::reject());
+                    }
+                }
+            }
+            match field.as_str() {
+                "address" => {
+                    let addr = String::from_utf8_lossy(&bytes).to_string();
+                    address = Some(parse_address(&addr).unwrap());
+                }
+                "key" => key = Some(String::from_utf8_lossy(&bytes).to_string()),
+                _ => (),
+            }
         }
-
-        if name == "file" {
-            file_content = bytes.clone(); // Store file content separately
-        }
-
-        fields.push((name, String::from_utf8_lossy(&bytes).to_string()));
     }
 
-    // Use `field_names` and `file_content` as needed
-    let json = json!({
-        "fields": fields,
-        "file_content": String::from_utf8_lossy(&file_content).to_string(),
-    });
+    let address = address.ok_or_else(|| warp::reject::reject())?;
+    let key = key.ok_or_else(|| warp::reject::reject())?;
+
+    let base = BaseRequest { address, key };
+    let value = FileData {
+        filename: filename.unwrap_or_default(),
+        content: file_content,
+    };
+    let set_req = SetRequest { base, value };
+
+    let res = set(*network, os, private_key, set_req).await.map_err(|e| {
+        Rejection::from(BadRequest {
+            message: format!("set error: {}", e),
+        })
+    })?;
+    let json = json!(res);
 
     Ok(warp::reply::json(&json))
 }
@@ -137,15 +174,23 @@ pub async fn handle_set(form: FormData, private_key: SecretKey) -> Result<impl R
 /// Set key with data as value on the subnet.
 pub async fn set(
     network: SdkNetwork,
-    address: Address,
-    key: String,
-    value: String,
+    os: ObjectStore,
     private_key: SecretKey,
-) -> anyhow::Result<serde_json::Value, Box<dyn Error>> {
-    // let signer = get_faucet_wallet(private_key, network)?;
-    // let config = network.subnet_config(Default::default())?;
-    // let amount = TokenAmount::from_whole(0);
-    // let tx = Account::transfer(&signer, address, config, amount).await?;
-    let tx = json!("{}");
+    set: SetRequest,
+) -> anyhow::Result<TxReceipt<Cid>, Box<dyn Error>> {
+    let mut signer = get_faucet_wallet(private_key, network)?;
+    let provider =
+        JsonRpcProvider::new_http(network.rpc_url()?, None, Some(network.object_api_url()?))?;
+    let file = async_tempfile::TempFile::new().await?;
+
+    let tx = os
+        .add(
+            &provider,
+            &mut signer,
+            set.key.as_str(),
+            file,
+            Default::default(),
+        )
+        .await?;
     Ok(tx)
 }
